92015328315d444062ccf16456dd4b35c0da5940
Timunas <ferreirinha_23@hotmail.com>
2017-07-30 02:28:21 +0100

    Added tests for TravianBot
    Removed BotRunner class. Implemented login and navigation test.

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,57 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test for login method
+ * ______________________
+ * created by Joao Suzana
+ */
+
+public class LoginTest {
+
+    private static Logger logger = LogManager.getLogger(LoginTest.class);
+
+    private TravianBot bot;
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void initBot(){
+        this.bot = new TravianBot("http://ts6.travian.com");
+
+        logger.info("Initializing...");
+        this.bot.init();
+    }
+
+    @Test
+    public void loginTest(){
+
+        WebElement element = this.bot.getDriver().findElement(By.className("login"));
+        assertThat("Didn't initialized properly!",element,is(notNullValue()));
+
+        logger.info("Login in...");
+        this.bot.login("Vlad", "1234cenas");
+
+        element = this.bot.getDriver().findElement(By.className("village1"));
+        assertThat("Didn't logged in properly!",element,is(notNullValue()));
+    }
+
+    @After
+    public void destroyBot(){
+        logger.info("Destroying Bot...");
+        this.bot.close();
+        logger.info("Finished Test!");
+    }
+}
---
+++ src/test/java/NavigationTest.java
@@ -0,0 +1,90 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test for navigation methods
+ * ______________________
+ * created by Joao Suzana
+ */
+
+public class NavigationTest {
+
+    private static Logger logger = LogManager.getLogger(NavigationTest.class);
+
+    private TravianBot bot;
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void initBot(){
+        this.bot = new TravianBot("http://ts6.travian.com");
+
+        logger.info("Initializing...");
+        this.bot.init();
+    }
+
+    @Test
+    public void coreTest(){
+        //Validate initialization
+        WebElement element = this.bot.getDriver().findElement(By.className("login"));
+        assertThat("Didn't initialized properly!",element,is(notNullValue()));
+
+        //Validate Login
+        logger.info("Login in...");
+        this.bot.login("Vlad", "1234cenas");
+        element = this.bot.getDriver().findElement(By.className("village1"));
+        assertThat("Didn't logged in properly!",element,is(notNullValue()));
+
+
+        bot.getNavigationBroker().goToResourcesView();
+        element = this.bot.getDriver().findElement(By.className("village1"));
+        assertThat("Didn't change to resources view!",element,is(notNullValue()));
+
+        bot.getNavigationBroker().goToInsideVillageView();
+        element = this.bot.getDriver().findElement(By.className("village2"));
+        assertThat("Didn't change to inside village view!",element,is(notNullValue()));
+
+        bot.getNavigationBroker().goToMap();
+        element = this.bot.getDriver().findElement(By.className("map"));
+        assertThat("Didn't change to Map!",element,is(notNullValue()));
+
+        bot.getNavigationBroker().goToReports();
+        element = this.bot.getDriver().findElement(By.className("reports"));
+        assertThat("Didn't change to reports view!",element,is(notNullValue()));
+
+        bot.getNavigationBroker().goToStatistics();
+        element = this.bot.getDriver().findElement(By.className("statistics"));
+        assertThat("Didn't change to statistics view!",element,is(notNullValue()));
+
+        bot.getNavigationBroker().goToMessages();
+        element = this.bot.getDriver().findElement(By.className("messages"));
+        assertThat("Didn't change to messages!",element,is(notNullValue()));
+
+        bot.getNavigationBroker().goToReports();
+        element = this.bot.getDriver().findElement(By.className("reports"));
+        assertThat("Didn't change to reports view!",element,is(notNullValue()));
+
+        bot.getNavigationBroker().goToReports();
+        element = this.bot.getDriver().findElement(By.className("reports"));
+        assertThat("Didn't change to reports view!",element,is(notNullValue()));
+    }
+
+    @After
+    public void destroyBot(){
+        logger.info("Destroying Bot...");
+        this.bot.close();
+        logger.info("Finished Test!");
+    }
+}
